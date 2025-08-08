66111a3eadc163995061e768b281118d8a3e93c6
valles exequiel matias <exequielvalles21@gmai.com>
2025-08-08 01:51:37 -0300

    solucionando mucho mas

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno según el modo (development, production, etc.)
+  const env = loadEnv(mode, process.cwd());
 
-
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env, // Solo necesario si usás process.env.VITE_API_URL
+    },
+  };
+});
